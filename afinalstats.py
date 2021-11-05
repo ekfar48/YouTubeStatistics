e5eb74764aca7bc2bd5da2797f869dd0b7dcf1b4
ekfar <68909674+ekfar48@users.noreply.github.com>
2021-11-05 21:36:20 +0300

    Update afinalstats.py

--- afinalstats.py
+++ afinalstats.py
@@ -2,8 +2,8 @@ import os
 import discord
 from discord.ext import commands
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
+#from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 
